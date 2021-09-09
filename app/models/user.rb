db47d6e9c64ed5eea3374129c12b7935c86b7edf
roweeena <roe0320@gmail.com>
2021-09-09 21:31:34 +1000

    test deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, :email, presence: true
   validates :name, :email, uniqueness: true
    validates :name, length: { minimum: 2 }
